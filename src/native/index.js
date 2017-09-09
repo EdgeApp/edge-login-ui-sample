41203a62d6990cca8ba037b04e120f7a7d011a04
Allen Hujsak <allen@Allens-MacBook-Air-2.local>
2017-09-09 10:45:39 -0500

    set up to handle multipe apps from core ui.

--- src/native/index.js
+++ src/native/index.js
@@ -3,7 +3,12 @@ import { StyleSheet, Text, View } from 'react-native'
 
 // Airbitz core libraries:
 import { makeReactNativeContext } from 'airbitz-core-react-native'
-import { LoginScreen } from 'airbitz-core-js-ui'
+import {
+  LoginScreen,
+  ChangePasswordScreen,
+  ChangePinScreen,
+  ChooseTestAppScreen
+} from 'airbitz-core-js-ui'
 
 function setupCore () {
   return makeReactNativeContext({
@@ -18,20 +23,86 @@ function setupCore () {
 export class App extends Component {
   constructor (props) {
     super(props)
-    this.state = { context: null, account: null }
+    this.state = {
+      context: null,
+      account: null,
+      screen: 'login'
+    }
 
     // Creating the context is async, so we store it in our state:
     setupCore().then(context => this.setState(state => ({ ...state, context })))
   }
+  onLogin (error = null, accountObject) {
+    this.setState({
+      account: accountObject,
+      screen: 'chooseScreen'
+    })
+  }
+  changePin () {
+    this.setState({
+      screen: 'changePin'
+    })
+  }
+
+  changePassword () {
+    this.setState({
+      screen: 'changePassword'
+    })
+  }
+  recoverPassword () {
+    this.setState({
+      screen: 'login'
+    })
+  }
+
+  onCancelApp () {
+    this.setState({
+      screen: 'chooseScreen'
+    })
+  }
 
   render () {
-    const onLogin = account => this.setState(state => ({ ...state, account }))
-
+    if (this.state.screen === 'changePassword') {
+      return (
+        <View style={styles.container}>
+          <ChangePasswordScreen
+            accountObject={this.state.context}
+            onComplete={this.onCancelApp.bind(this)}
+          />
+        </View>
+      )
+    }
+    if (this.state.screen === 'changePin') {
+      return (
+        <View style={styles.container}>
+          <ChangePinScreen
+            accountObject={this.state.context}
+            onComplete={this.onCancelApp.bind(this)}
+            onCancel={this.onCancelApp.bind(this)}
+          />
+        </View>
+      )
+    }
+    if (this.state.screen === 'chooseScreen') {
+      return (
+        <View style={styles.container}>
+          <ChooseTestAppScreen
+            onChangePassword={this.changePassword.bind(this)}
+            onChangePin={this.changePin.bind(this)}
+            onRecoverPassword={this.recoverPassword.bind(this)}
+            onCancel={this.onCancelApp.bind(this)}
+          />
+        </View>
+      )
+    }
     // Once the context is ready, we can show the login screen:
     return (
       <View style={styles.container}>
         {this.state.context
-          ? <LoginScreen context={this.state.context} onLogin={onLogin} />
+          ? <LoginScreen
+            context={this.state.context}
+            onLogin={this.onLogin.bind(this)}
+            />
           : <Text style={styles.welcome}>Loading</Text>}
       </View>
     )
