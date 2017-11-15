797ce96b85ffab8965afba8179463e89df1da11b
Allen Hujsak <allenhujsak@Allens-MacBook-Air.local>
2017-11-14 18:08:08 -0800

    react linked the gradient library

--- android/app/src/main/java/com/airbitzcoresample/MainApplication.java
+++ android/app/src/main/java/com/airbitzcoresample/MainApplication.java
@@ -3,6 +3,7 @@ package com.airbitzcoresample;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.BV.LinearGradient.LinearGradientPackage;
 import co.airbitz.AbcCoreJsUi.AbcCoreJsUiPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.bitgo.randombytes.RandomBytesPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new LinearGradientPackage(),
             new AbcCoreJsUiPackage(),
             new VectorIconsPackage(),
             new RandomBytesPackage(),
